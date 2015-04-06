4b1b453e5bdb9faf4389a877f49e6f3c1a522c5d
TirtaWR <tirtawening@gmail.com>
2015-04-06 15:51:58 +0700

    trakhir banget

--- src/RSSep/MainGUI.java
+++ src/RSSep/MainGUI.java
@@ -3,15 +3,7 @@ package RSSep;
 public class MainGUI {
 	public static void main(String[] args) {
 		tubesOOPDemo demo = null;
-		try{
-			demo = new tubesOOPDemo();
-			if(demo == null){
-				throw new customException();
-			}
-			demo.runGUI();
-		}
-		catch(customException e){
-			
-		}
+		demo = new tubesOOPDemo();
+		demo.runGUI();
 	} 
 }
